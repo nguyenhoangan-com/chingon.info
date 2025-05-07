9f5ea362135bb5be7a339aca40657c6401778b33
nguyenhoangan <121211419+nguyenhoangan-monkey@users.noreply.github.com>
2025-05-07 01:13:59 -0400

    add animejs as animation engine

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,7 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("src/guide");
   eleventyConfig.addPassthroughCopy("src/css");
   eleventyConfig.addPassthroughCopy("src/asset");
+  eleventyConfig.addPassthroughCopy("src/js");
 
   return {
     dir: {
